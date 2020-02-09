97a6c898dff0d83023b456bef3d82a23fa5ad9e3
Tan Jun Ming <jtan@csumb.edu>
2020-02-08 19:04:37 -0800

    Add items to grade view

--- app/src/main/java/com/group/a/gradeapp/GradesView.java
+++ app/src/main/java/com/group/a/gradeapp/GradesView.java
@@ -1,6 +1,8 @@
 package com.group.a.gradeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,15 @@ public class GradesView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grades_view);
+
+        RecyclerView recycler_view = findViewById(R.id.gradeview_list);
+
+        RecyclerView.LayoutManager layout_manager = new LinearLayoutManager(this);
+        recycler_view.setLayoutManager(layout_manager);
+
+        java.lang.String[] grades = {};
+
+        GradeViewAdapter grade_adapter = new GradeViewAdapter(grades);
+        recycler_view.setAdapter(grade_adapter);
     }
 }
