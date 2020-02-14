ac0406e5bfbd803b81baf17e0dc924063b34deef
Tan Jun Ming <jtan@csumb.edu>
2020-02-13 22:54:03 -0800

    Finish Spinner in Enroll page

--- app/src/main/java/com/group/a/gradeapp/EnrollCourseActivity.java
+++ app/src/main/java/com/group/a/gradeapp/EnrollCourseActivity.java
@@ -3,7 +3,9 @@ package com.group.a.gradeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import com.group.a.gradeapp.DB.Course;
@@ -18,7 +20,7 @@ public class EnrollCourseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enroll_course);
 
-        Spinner course_spinner = (Spinner) findViewById(R.id.course_spinner);
+        final Spinner course_spinner = (Spinner) findViewById(R.id.course_spinner);
 
         ArrayAdapter<Course> adapter = new ArrayAdapter<Course>(this,
                 android.R.layout.simple_spinner_item, get_course_array());
@@ -26,6 +28,16 @@ public class EnrollCourseActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         course_spinner.setAdapter(adapter);
+
+        Button enroll_course_button = findViewById(R.id.enroll_course_button);
+
+        enroll_course_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Course item = (Course) course_spinner.getSelectedItem();
+                utils.display_toast(getApplicationContext(), item.toString() + " selected");
+            }
+        });
 
     }
 
