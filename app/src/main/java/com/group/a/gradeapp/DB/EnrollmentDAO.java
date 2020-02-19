bcf8fb5e2836770a6ef954a4f8b40c72fe56f3c7
Tan Jun Ming <jtan@csumb.edu>
2020-02-18 21:50:36 -0800

    Add EnrollmentDAO (because android studio wants me to before I can pull)

---
+++ app/src/main/java/com/group/a/gradeapp/DB/EnrollmentDAO.java
@@ -0,0 +1,42 @@
+package com.group.a.gradeapp.DB;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+
+import java.util.List;
+
+@Dao
+public interface EnrollmentDAO {
+    @Insert
+    void AddEnrollment( Enrollment Enrollment);
+
+    @Update
+    void update(Enrollment... Enrollments);
+    @Delete
+    void delete(Enrollment Enrollment);
+
+    @Query("select * from " + AppDatabase.ENROLLMENT_TABLE)
+    List<Enrollment> getEnrollments();
+
+    @Query("select * from AssignmentTable where AssTitle = :AssTitle")
+    Enrollment getEnrollmentByID(String AssTitle);
+
+    @Insert
+    void addEnrollment(Enrollment Enrollment);
+
+
+
+
+// log record queries
+    @Query("select * from LogRecord order by time desc")
+    List<LogRecord> getAllLogRecords();
+
+    @Insert
+    void addLogRecord(LogRecord rec);
+
+}
+
