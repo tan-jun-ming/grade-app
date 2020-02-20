9758ddf530d78fafc3e4f9cc4bf8c489a7abb4e2
Yessica Torrez Hernandez <jessh2182@gmail.com>
2020-02-20 12:36:20 -0800

    logrecord set

--- app/src/test/java/com/group/a/gradeapp/unitLogRecordTest.java
+++ app/src/test/java/com/group/a/gradeapp/unitLogRecordTest.java
@@ -1,8 +1,34 @@
 package com.group.a.gradeapp;
 
+import com.group.a.gradeapp.DB.LogRecord;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+
 public class unitLogRecordTest {
 
-    public void addLogRecord(){
+    @Test
+    public void createLogRecord(){
+        LogRecord logRecord = new LogRecord();
+        assertNotNull(logRecord);
+    }
 
+    @Test
+    public void setLogRecord(){
+        LogRecord logRecord = new LogRecord();
+        logRecord.setUserID(8611);
+        logRecord.setTime(02);
+        logRecord.setTransaction_type("transaction");
+        logRecord.setUsername("GroupA");
+        logRecord.setDetailed_message("adding record");
+
+        assertTrue(logRecord.getUserID() == 8611 &&
+                logRecord.getTime() == 02 &&
+                logRecord.getTransaction_type() == "transaction" &&
+                logRecord.getUsername() == "GroupA" &&
+                logRecord.getDetailed_message() == "adding record");
     }
 }
